4a70a15dc176f80166880e5aaf414cc7e09a2458
eschiller <ericgschiller@gmail.com>
2017-03-26 14:00:57 -0400

    svg import/export tested and working

--- data/fpdatamgr.py
+++ data/fpdatamgr.py
@@ -2,8 +2,18 @@ import fpdata
 import json
 import numpy as np
 import xml.etree.ElementTree
+from xml.etree.ElementTree import Element, SubElement, tostring, XML
+import xml.dom.minidom
 import re
 
+
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = xml.etree.ElementTree.tostring(elem, 'utf-8')
+    reparsed = xml.dom.minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="\t")
 
 def get_svg_layer_paths(file_path, layer="layer1"):
     e = xml.etree.ElementTree.parse(file_path).getroot()
@@ -34,6 +44,10 @@ class fpdatamgr:
         return return_string
 
 
+    def get_data_fp(self, index):
+        return self.fplist[index]
+
+
     def add_fp(self, fp):
         '''
         Adds the passed fpdata object to the end of the fplist list
@@ -59,11 +73,14 @@ class fpdatamgr:
             first_command = path.pop(0)
             print("first command is " + first_command)
             if first_command == "m":
-                cursor["x"] += float(path.pop(0))
-                cursor["y"] += float(path.pop(0))
+                print("command is m, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
+
+                cursor["x"] = float(path.pop(0))
+                cursor["y"] = float(path.pop(0))
                 start_coord["x"] = cursor["x"]
                 start_coord["y"] = cursor["y"]
             if first_command == "M":
+                print("command is M, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                 cursor["x"] = float(path.pop(0))
                 cursor["y"] = float(path.pop(0))
                 start_coord["x"] = cursor["x"]
@@ -75,6 +92,7 @@ class fpdatamgr:
 
                 #wall segment from current cursor to start
                 if (command == "z") or (command == "Z"):
+                    print("command is z, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     awall.append(start_coord["x"])
@@ -83,6 +101,7 @@ class fpdatamgr:
 
                 #wall segment from current cursor to relative location
                 elif command == "l":
+                    print("command is l, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["x"] += float(path.pop(0))
@@ -93,6 +112,7 @@ class fpdatamgr:
 
                 #wall segment from current cursor to absolute location
                 elif command == "L":
+                    print("command is L, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["x"] = float(path.pop(0))
@@ -103,6 +123,7 @@ class fpdatamgr:
 
                 #wall segment from cursor to relative horizontal loc
                 elif command == "h":
+                    print("command is h, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["x"] += float(path.pop(0))
@@ -112,6 +133,7 @@ class fpdatamgr:
 
                 #wall segment from cursor to absolut horizontal loc
                 elif command == "H":
+                    print("command is H, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["x"] = float(path.pop(0))
@@ -121,6 +143,7 @@ class fpdatamgr:
 
                 #wall segment from cursor to relative vertical loc
                 elif command == "v":
+                    print("command is v, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["y"] += float(path.pop(0))
@@ -130,12 +153,16 @@ class fpdatamgr:
 
                 #wall segment from cursor to absolute vertical loc
                 elif command == "V":
+                    print("command is V, cursor at " + str(cursor["x"]) + "," + str(cursor["y"]))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     cursor["y"] = float(path.pop(0))
                     awall.append(cursor["x"])
                     awall.append(cursor["y"])
                     walls.append(awall)
+
+                else:
+                    print("Error: unknown svg path command: " + command)
 
         #create a new floorplan and add a path for each wall we've parsed from the svg
         fp1 = fpdata.fpdata()
@@ -148,6 +175,77 @@ class fpdatamgr:
 
         return 0
 
+
+    def export_svg(self, index, filename):
+        target_fp = self.get_data_fp(index)
+        fp_paths = target_fp.get_paths()
+
+        top = Element('svg', {"xmlns:dc": "http://purl.org/dc/elements/1.1/",
+                              "xmlns:cc": "http://creativecommons.org/ns#",
+                              "xmlns:rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                              "xmlns:svg": "http://www.w3.org/2000/svg",
+                              "xmlns": "http://www.w3.org/2000/svg",
+                              "xmlns:sodipodi": "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
+                              "xmlns:inkscape": "http://www.inkscape.org/namespaces/inkscape",
+                              "width": "180mm",
+                              "height": "180mm",
+                              "viewBox": "0 0 180 180",
+                              "version": "1.1",
+                              "id": "svg8",
+                              "inkscape:version": "0.92.0 r",
+                              "sodipodi:docname": filename + ".svg",
+                              "inkscape:export-filename": "./" + filename + ".png",
+                              "inkscape:export-xdpi": "96",
+                              "inkscape:export-ydpi": "96"
+                              })
+        defs = SubElement(top, "defs", {"id": "defs2"})
+        sodipodi = SubElement(top, "sodipodi:namedview", {"id": "base",
+                                                          "pagecolor": "#ffffff",
+                                                          "bordercolor": "#666666",
+                                                          "borderopacity": "1.0",
+                                                          "inkscape:pageopacity": "0.0",
+                                                          "inkscape:pageshadow": "2",
+                                                          "inkscape:zoom": "1.0",
+                                                          "inkscape:cx": "100.0",
+                                                          "inkscape:cy": "100.0",
+                                                          "inkscape:document-units": "mm",
+                                                          "inkscape:current-layer": "layer1",
+                                                          "showgrid": "false",
+                                                          "inkscape:window-width": "2000",
+                                                          "inkscape:window-height": "900",
+                                                          "inkscape:window-x": "0",
+                                                          "inkscape:window-y": "1",
+                                                          "inkscape:window-maximized": "0"
+                                                          })
+
+        metadata = SubElement(top, "metadata", {"id": "metadata5"})
+        rdf = SubElement(metadata, "rdf:RDF")
+        ccWork = SubElement(rdf, "cc:Work", {"rdf:about": ""})
+        dcformat = SubElement(ccWork, "dc:format").text = "image/svg+xml"
+        dctype = SubElement(ccWork, "dc:type", {"rdf:resource": "http://purl.org/dc/dcmitype/StillImage"})
+        dctitle = SubElement(ccWork, "dc:title")
+
+        g1 = SubElement(top, "g", {"inkscape:label": "Layer 1",
+                                   "inkscape:groupmode": "layer",
+                                   "id": "layer1",
+                                   "style": "display:inline"})
+
+        pindex = 3600
+        paths = []
+        for some_path in fp_paths:
+            if some_path['pathtype'] == 1.0:
+                paths.append(SubElement(g1, "path", {
+                    "style": "fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1",
+                    "d": "M " + str(some_path['p1x']) + "," + str(some_path['p1y']) + " L " + str(
+                        some_path['p2x']) + "," + str(some_path['p2y']),
+                    "id": "path" + str(pindex),
+                    "inkscape:connector-curvature": "0"
+                    }))
+                pindex += 1
+
+        with open("./" + filename + ".svg", 'w') as f:
+            f.write(prettify(top))
+            f.write("\n")
 
 
     def to_json_file(self, filename="output.json"):
@@ -185,7 +283,6 @@ class fpdatamgr:
 
     def generate_test_set(self, size=100):
 
-        #todo - probably need to pass a "size" param and return a set of that size
         fp1 = fpdata.fpdata()
         fp1.add_path(1, 30, 30, 30, 130)
         fp1.add_path(1, 30, 130, 130, 130)
